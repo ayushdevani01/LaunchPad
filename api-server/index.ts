bb041630ee6203df90a020c3d36f4ff65674a241
MIHIR2006 <mihirgoswami2006@gmail.com>
2025-12-18 22:52:38 +0530

    Add launchpad core implementation

---
+++ api-server/index.ts
@@ -0,0 +1,85 @@
+import express from 'express'
+import { generateSlug } from 'random-word-slugs'
+import { ECSClient, RunTaskCommand } from '@aws-sdk/client-ecs'
+import { Server } from 'socket.io'
+import Redis from 'ioredis'
+
+const app = express()
+const PORT = 9000
+
+const subscriber = new Redis('')
+
+const io = new Server({ cors: { origin: '*' } })
+
+io.on('connection', socket => {
+    socket.on('subscribe', channel => {
+        socket.join(channel)
+        socket.emit('message', `Joined ${channel}`)
+    })
+})
+
+io.listen(9002)
+console.log('Socket Server 9002')
+
+const ecsClient = new ECSClient({
+    region: '',
+    credentials: {
+        accessKeyId: '',
+        secretAccessKey: ''
+    }
+})
+
+const config = {
+    CLUSTER: '',
+    TASK: ''
+}
+
+app.use(express.json())
+
+app.post('/project', async (req, res) => {
+    const { gitURL, slug } = req.body
+    const projectSlug = slug ? slug : generateSlug()
+
+    const command = new RunTaskCommand({
+        cluster: config.CLUSTER,
+        taskDefinition: config.TASK,
+        launchType: 'FARGATE',
+        count: 1,
+        networkConfiguration: {
+            awsvpcConfiguration: {
+                assignPublicIp: 'ENABLED',
+                subnets: ['', '', ''],
+                securityGroups: ['']
+            }
+        },
+        overrides: {
+            containerOverrides: [
+                {
+                    name: 'builder-image',
+                    environment: [
+                        { name: 'GIT_REPOSITORY__URL', value: gitURL },
+                        { name: 'PROJECT_ID', value: projectSlug }
+                    ]
+                }
+            ]
+        }
+    })
+
+    await ecsClient.send(command);
+
+    return res.json({ status: 'queued', data: { projectSlug, url: `http://${projectSlug}.localhost:8000` } })
+
+})
+
+async function initRedisSubscribe() {
+    console.log('Subscribed to logs....')
+    subscriber.psubscribe('logs:*')
+    subscriber.on('pmessage', (pattern, channel, message) => {
+        io.to(channel).emit('message', message)
+    })
+}
+
+
+initRedisSubscribe()
+
+app.listen(PORT, () => console.log(`API Server Running..${PORT}`))
